3f6cee0320c0b2795e7c1e9658b265796c22e319
julianarpaz <julianarpaz@gmail.com>
2022-06-28 16:20:45 -0300

    Adicionando função de carregamento de imagens

--- game.py
+++ game.py
@@ -6,6 +6,8 @@ class Game:
     def __init__(self, board, screenSize):
         self.board = board
         self.screenSize = screenSize
+        self.pieceSize = self.screenSize[0] // self.board.getSize()[1], self.screenSize[1] // self.board.getSize()[0]
+        self.loadImages
 
     def run(self):
         pygame.init()
@@ -15,9 +17,25 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            
+            self.draw()
             pygame.display.flip()
         pygame.quit()
+        
+    def draw(self):
+        topLeft = (0, 0)
+        for row in range(self.board.getSize()[0]):
+            for col in range(self.board.getSize()[1]):
+                image = self.images()
+                self.screen.blit(image, topLeft)
+    
+    def loadImages(self):
+        self.images={}
+        for fileName in os.listdir("images"):
+            if(not fileName.endswith(".png")):
+                continue
+            image = pygame.Image.load(r"images/" + fileName)
+            image = pygame.transform.scale(image, self.pieceSize)
+            self.images[fileName.split(".")[0]] = image
 
 
 
